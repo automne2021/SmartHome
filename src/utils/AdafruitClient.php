567b2db490e1f9921c9aeeea339998b8ae7196a6
automne2021 <vhkhanhvy25082601@gmail.com>
2025-04-25 00:37:30 +0700

    db

--- src/utils/AdafruitClient.php
+++ src/utils/AdafruitClient.php
@@ -1,34 +1,49 @@
 <?php
-class AdafruitClient {
+class AdafruitClient
+{
     private $apiKey;
     private $username;
-    
-    public function __construct($apiKey, $username) {
+
+    public function __construct($apiKey, $username)
+    {
         $this->apiKey = $apiKey;
         $this->username = $username;
     }
-    
-    public function getData($feed) {
+
+    public function getData($feed)
+    {
         $url = "https://io.adafruit.com/api/v2/{$this->username}/feeds/{$feed}/data";
         $ch = curl_init();
-        
+
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HTTPHEADER, [
             "X-AIO-Key: {$this->apiKey}",
             'Content-Type: application/json'
         ]);
-        
+        // Add timeout settings
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+
         $response = curl_exec($ch);
+
+        // Check for timeout or error
+        if (curl_errno($ch)) {
+            error_log('Curl error when contacting Adafruit: ' . curl_error($ch));
+            curl_close($ch);
+            return [];
+        }
+
         curl_close($ch);
-        
+
         return json_decode($response, true);
     }
-    
-    public function sendData($feed, $value) {
+
+    public function sendData($feed, $value)
+    {
         $url = "https://io.adafruit.com/api/v2/{$this->username}/feeds/{$feed}/data";
         $data = json_encode(['value' => $value]);
-        
+
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -38,11 +53,21 @@ class AdafruitClient {
             "X-AIO-Key: {$this->apiKey}",
             'Content-Type: application/json'
         ]);
-        
+        // Add timeout settings
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+
         $response = curl_exec($ch);
+
+        // Check for timeout or error
+        if (curl_errno($ch)) {
+            error_log('Curl error when sending to Adafruit: ' . curl_error($ch));
+            curl_close($ch);
+            return false;
+        }
+
         curl_close($ch);
-        
+
         return json_decode($response, true);
     }
 }
-?>
